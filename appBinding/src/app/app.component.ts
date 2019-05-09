bcc28f190af774c8433febcac9d146635fbc1b43
camilocartagena <camilocartagena@gmail.com>
2019-05-09 02:38:10 -0500

    add event

--- appBinding/src/app/app.component.ts
+++ appBinding/src/app/app.component.ts
@@ -9,4 +9,9 @@ export class AppComponent {
   title = 'appBinding';
   developer = 'Camilo Cartagena';
   urlImage = 'https://cdn.worldvectorlogo.com/logos/angular-3.svg'
+
+  onSave(){  
+    console.log("Save operation is clicked!");  
+  }  
+
 }
