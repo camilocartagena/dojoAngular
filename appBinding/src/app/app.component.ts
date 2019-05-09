81cb7b4cdb9cc9a715f4ef04da049dceb072c156
camilocartagena <camilocartagena@gmail.com>
2019-05-09 04:10:43 -0500

    example json read

--- appBinding/src/app/app.component.ts
+++ appBinding/src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import SampleJson from '../assets/sampleJson.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+
+  constructor() {
+    console.log('Reading local json files');
+    console.log(SampleJson);
+   }
+   
+   ngOnInit() {}
+
   title = 'appBinding';
   developer = 'Camilo Cartagena';
   urlImage = 'https://cdn.worldvectorlogo.com/logos/angular-3.svg'
