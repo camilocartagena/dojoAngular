a07a058d4096fb65ad9c46b11c5f37ce47092628
camilocartagena <camilocartagena@gmail.com>
2019-05-09 02:25:41 -0500

    add property

--- appBinding/src/app/app.component.ts
+++ appBinding/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'appBinding';
   developer = 'Camilo Cartagena';
+  urlImage = 'https://cdn.worldvectorlogo.com/logos/angular-3.svg'
 }
